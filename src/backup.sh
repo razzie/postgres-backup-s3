6091ecde9be41ea3858efd53609dca296e560c0e
Gábor Görzsöny <gabor@gorzsony.com>
2022-08-11 22:30:52 +0200

    optimized pg_dump

--- src/backup.sh
+++ src/backup.sh
@@ -67,7 +67,7 @@ echo "Creating backup of $POSTGRES_DATABASE database..."
 pg_dump --format=custom \
         $conn_opts \
         $PGDUMP_EXTRA_OPTS \
-        > db.dump
+        -f db.dump
 
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
 s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
